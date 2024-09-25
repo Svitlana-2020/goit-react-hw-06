9bb84a6525a2a0a38daedd8d3ed789797deffcce
Svitlana <160290680+Svitlana-2020@users.noreply.github.com>
2024-09-26 00:02:28 +0100

    reduxpersist

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
     </Provider>
   </StrictMode>,
 )
