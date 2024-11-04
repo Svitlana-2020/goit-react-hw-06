7dbeeadf38f212a8de5803e53820b2545a68592b
Svitlana <160290680+Svitlana-2020@users.noreply.github.com>
2024-11-04 13:17:28 +0000

    config

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,39 @@ import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlice';
 import changeFilter from './filtersSlice';
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer)
+
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedReducer,
     filters: changeFilter,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      }})
 });
+
+export const persistor = persistStore(store)
 
 // const rootReducer = (state = initialState, action) => {
 //   return state;
